379f2c46444eb8bf9a85c83fa0e5230fd97f31cf
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2020-12-14 21:24:07 -0500

    add first test the project

---
+++ StartingProject/EmployeesApp/EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
@@ -0,0 +1,28 @@
+﻿using EmployeesApp.Validaton;
+using Xunit;
+
+namespace EmployeesApp.Tests.Validation
+{
+    public class AccountNumberValidationTests
+    {
+        private readonly AccountNumberValidation _validation;
+        public AccountNumberValidationTests()
+        {
+            _validation = new AccountNumberValidation();
+        }
+
+        [Fact]
+        public void IsValid_ValidAccountNumber_ReturnsTrue()
+        {
+            Assert.True(_validation.IsValid("123-4543234576-23"));
+        }
+
+        [Theory]
+        [InlineData("1234-3454565676-23")]
+        [InlineData("12-3454565676-23")]
+        public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse(string accountNumber)
+        {
+            Assert.False(_validation.IsValid(accountNumber));
+        }
+    }
+}
